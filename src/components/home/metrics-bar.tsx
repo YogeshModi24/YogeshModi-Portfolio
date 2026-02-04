bc78fdb0aef43e4e788e5746e3343dbeee3f4522
YogeshModi24 <info.yogessh@gmail.com>
2026-02-05 00:37:40 +0530

    chore: Remove unused `Info` import from `metrics-bar.tsx`.

--- src/components/home/metrics-bar.tsx
+++ src/components/home/metrics-bar.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { motion } from 'framer-motion'
-import { Info } from 'lucide-react'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 
 const stats = [
